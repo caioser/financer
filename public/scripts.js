be53a1a593e28056db375511cde02b78d5b597ef
Caio Sergio <csrmyo@gmail.com>
2021-03-24 23:31:43 -0300

    Decided to follow Organizze App layout model, making scripts to simulate the features of layout. In this commit, working on the script that handle the numbers digited on input

--- public/scripts.js
+++ public/scripts.js
@@ -7,6 +7,48 @@ function load() {
         function(){setBool(e.id)},
         false)
     );
+
+    let backv = document.querySelector('#back-value');
+    let frontv = document.querySelector('#front-value');
+    let digited = [];
+    frontv.addEventListener(
+        'click',
+        function() {
+            let a = this.value.length;
+            this.setSelectionRange(a, a);
+        },
+        false
+    );
+    frontv.addEventListener(
+        'input',
+        function() {
+            let dft = ["0", ",", "0", "0"];
+            let current = frontv.value.split("");
+            if ( current > 4 ) {
+                digited.push(current.pop());
+                current.splice(-digited.length, digited.length);
+                frontv.value = current.join("");
+            }
+            console.log(current);
+            console.log(digited);
+
+            
+
+        },
+        false
+    );
+
+    backv.addEventListener(
+        'input',
+        function() {
+            /* receive front and respond*/
+            
+            
+            backv.setAttribute('value', backv.value);
+            console.log(backv.getAttribute('value'));
+        },
+        false
+    );
 }
 
 function setBool(id) {
@@ -14,11 +56,13 @@ function setBool(id) {
     let hidden = document.getElementsByName(id)[0];
     if (hidden.getAttribute("value") === "false") {
         hidden.setAttribute("value", "true");
-        trigger.innerHTML = "true";
+        //trigger.innerHTML = "true";
+        trigger.className = "hidden green";
     } else {
         hidden.setAttribute("value", "false");
-        trigger.innerHTML = "false";
+        //trigger.innerHTML = "false";
+        trigger.className = "hidden red";
     }
 
-    //console.log('trocou para ');
-}
+    //console.log('trocou  para ');
+}    
