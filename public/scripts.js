78e719ff04ea182db3f33a03b5a30e52d083e35a
Caio Sergio <csrmyo@gmail.com>
2021-03-27 23:58:36 -0300

    Completed 'slider' containing expense, income and tranfer with all animations, transactions, colors and icons. Link with icon backing to home. Value input ok and Status input too

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,6 @@
 // PERSONALIZING
 let timestamp = new Date();
+let c = (query)=>document.querySelector(query);
 let logtime = `${timestamp.getHours()}:${timestamp.getMinutes()}:${timestamp.getSeconds()}`;
 let como = tx=>console.log(tx);
 como('focused '+logtime);
@@ -8,15 +9,70 @@ function autof5(){
     
 }
 
+let to = {
+    lightBlack:['#222325','34,35,37'],
+    mainBlack:['#1b1b1b','27,27,27'],
+    red:['#d3322a','211,50,42'],
+    green:['#006813','0,104,19'],
+    gray:['#464646','70,70,70'],
+    fontWhite:['#f8ffff','248,255,255'],
+    fontGray:['#b8c2ca','184,194,202'],
+    fontCyan:['#0fbbf2','15,187,242'],
+    fontGreen:['#25c773','37,199,115'],
+    fontRed:['#fa7170','250,113,112'],
+}
+function paint(element, color, font=false, opacity=1) {
+    if (font) {
+        return c(element).style.color = `rgba(${color[1]},${opacity})`;
+    }
+    return c(element).style.backgroundColor = `rgba(${color[1]},${opacity})`;
+}
+
 
 
 // EVENT LISTENERS
 function load() {
-    window.addEventListener('focus', autof5, false);
+    // window.addEventListener('focus', autof5, false);
     el.map(e=>e.addEventListener('click', function(){setBool(e.id)}, false));
+    op.map(e=>e.addEventListener('click', slideType, false));
     frontv.addEventListener('click', inputToEnd, false);
     frontv.addEventListener('input', moneyFormatWhenTyping, false);
+    status.addEventListener('click', flip, false);
 }
+
+// OPERATION expense, incoming and transfer input
+let op = Object.values(document.querySelectorAll('#op-line1 div'));
+op.map((e)=>paint('#'+e.id, to.fontWhite, true, 0.5));
+paint('#ex', to.fontWhite, true);
+let opIndex = 1;
+let setBallMoveDefaultDistance = c('#op-line1 a').offsetWidth*2;
+function slideType() {
+    op.map((e)=>paint('#'+e.id, to.fontWhite, true, 0.5));
+    switch (this.innerHTML){
+        case 'Incoming':
+            opIndex = 2;
+            paint('#bkgrd', to.green);
+            paint('#in', to.fontWhite, true);
+            c('#op-icon-img').src = '/income.png';
+            break;
+        case 'Transfer':
+            opIndex = 3;
+            paint('#bkgrd', to.gray);
+            paint('#tr', to.fontWhite, true);
+            c('#op-icon-img').src = '/transfer.png';
+            break;
+        default:
+            opIndex = 1;
+            paint('#bkgrd', to.red);
+            paint('#ex', to.fontWhite, true);
+            c('#op-icon-img').src = '/expense.png';
+            break;
+    }
+    c('#move').style.width = `${setBallMoveDefaultDistance*opIndex}px`;
+}
+
+
+
 
 
 // VALUE INPUT
@@ -62,6 +118,37 @@ function moneyFormatWhenTyping() {
     }
     current = historic;
     frontv.value = current;
+}
+
+
+// STATUS INPUT
+let status = c('#status-container');
+let statusTumbling = [
+    { transform: 'scale(0.5,0.5)'},
+    { transform: 'scale(1,1)'},
+    { transform: 'scale(0.8,0.8)'},
+    { transform: 'scale(1,1)'}
+];
+  
+let statusTiming = {
+    duration: 200,
+    iterations: 1
+}
+
+function flip() {
+    como("click");
+    if (c('#status-container input').value === 'false') {
+
+        // c('#status').style.transform = 'rotate(-180deg)';
+        c('#status').src = "/like.png";
+        c('#status-container input').value= 'true';
+        c('#status').animate(statusTumbling, statusTiming);
+    } else {
+        // c('#status').style.transform = 'rotate(360deg)';
+        c('#status').src = "/unlike.png";
+        c('#status-container input').value= 'false';
+        c('#status').animate(statusTumbling, statusTiming);
+    }
 }
 
 
